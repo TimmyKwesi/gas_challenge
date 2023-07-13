432835731ae8d4e89bad1be2846a88d5a470ce18
Timothy Ofori <timmysteinx@gmail.com>
2023-07-13 20:41:39 +0000

    initial commit

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
     
     //Function to check for sum of array
     //No changes required in this function
@@ -24,6 +24,11 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+      uint a = numbers.length;
+      unchecked {
+        for (uint i = 0; i < a; ++i) {
+            numbers[i] = 0;
+        }
+      }
     }
  }
